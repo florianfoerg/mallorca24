0dcf1fa6db1f7026095e21834afd523a359df9e2
root <root@mallorca24.fritz.box>
2023-05-18 18:43:29 +0000

    fixed run script setup

--- server/setup/run.sh
+++ server/setup/run.sh
@@ -12,5 +12,5 @@ sudo apt-get install -y python3 python3-pip
 pip3 install psycopg2-binary requests
 
 # Run Python scripts
-python3 initialize_hotels.py
-nohup python3 initialize_offers.py &
+python3 initialize_hotels_db.py
+nohup python3 initialize_offers_db.py &
